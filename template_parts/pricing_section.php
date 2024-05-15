e967d8ec579e41d6a41920e7e351457b0006b49e
admin <admin@admins-MacBook-Pro.local>
2024-05-15 10:48:29 +0300

    add pricing block

---
+++ template_parts/pricing_section.php
@@ -0,0 +1,44 @@
+<?php
+    $build_folder = get_template_directory_uri() . '/assets/';
+    $title = get_sub_field( 'title' );
+    $text = get_sub_field( 'text' );
+?>
+
+<section class="pricing-section" id="pricing">
+    <div class="container">
+        <div class="pricing-section__inner">
+            <?php display_main_top_section('h2',$title, $text) ?>
+
+            <?php if (have_rows('service')) : ?>
+                <ul class="pricing-section__services">
+                    <?php while (have_rows('service')) : the_row(); 
+                        $service_title = get_sub_field( 'service_title' );
+                        $service_price = get_sub_field( 'service_price' );
+                        ?>
+                        
+                        <li class="pricing-section__service">
+                            <span class="pricing-section__service-heading"><?php echo $service_title ?></span>
+
+                            <div class="pricing-section__service-price">
+                                from <?php echo $service_price ?>
+                            </div>
+
+                            <a class="pricing-section__service-btn btn" href="#">Order site</a>
+
+                            <?php if (have_rows('service_advantage')) : ?>
+                                <ul class="pricing-section__service-advantages">
+                                    <?php while (have_rows('service_advantage')) : the_row(); 
+                                        $service_adv = get_sub_field( 'service_adv' );
+                                        ?>
+                                        
+                                        <li class="pricing-section__service-advantage"><?php echo $service_adv ?></li>
+                                    <?php endwhile; ?>
+                                </ul>
+                            <?php endif; ?>
+                        </li>
+                    <?php endwhile; ?>
+                </ul>
+            <?php endif; ?>
+        </div>
+    </div>
+</section>
